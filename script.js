f13f9f64bfbc2cd5ddb100791839454222f90aa3
cobblesteve-dev <channelediting521@gmail.com>
2025-01-20 13:09:05 -0600

    Add Settings Template

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ function play() {
         window.location.href = newUrl;
     }
 }
+
+function settings() {
+    const popup = document.getElementById('settings-cont');
+    const overlay = document.getElementById('overlay');
+    if (popup && overlay) {
+        overlay.classList.add('visible');
+        popup.style.display = 'block';
+        requestAnimationFrame(() => {
+            popup.classList.add('scale-in');
+        });
+    }
+}
+
+function close() {
+    const popup = document.getElementById('settings-cont');
+    const overlay = document.getElementById('overlay');
+    if (popup && overlay) {
+        overlay.classList.remove('visible');
+        popup.classList.remove('scale-in');
+        popup.classList.add('scale-out');
+        popup.addEventListener(
+            'animationend',
+            function onAnimationEnd() {
+                popup.style.display = 'none';
+                popup.classList.remove('scale-out');
+                popup.removeEventListener('animationend', onAnimationEnd);
+            }
+        );
+    }
+}
